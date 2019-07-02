800bf67e03a9d80184ad7070ebeb3d1dbd1bd1ff
rishickesh <rishickeshramesh@gmail.com>
2019-07-02 13:54:29 +0530

    Update encoding_layer.py

--- Encoding_Layer_3/encoding_layer.py
+++ Encoding_Layer_3/encoding_layer.py
@@ -2,7 +2,7 @@ import torch
 from torch import nn
 from torch.nn.utils.rnn import pack_padded_sequence, pad_packed_sequence
 
-from embedding import RNN
+#from embedding import RNN
 
 
 class EncodingLayerRNN(nn.Module):
