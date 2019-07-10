82a22c954c0ef2b8f1d974f86c792274af14db0f
deepsahni11 <na16b110@smail.iitm.ac.in>
2019-07-10 16:36:29 +0530

    embedding layer 1 updated

--- Embedding_Layer_1/embedding.py
+++ Embedding_Layer_1/embedding.py
@@ -3,17 +3,22 @@ from torch.nn.utils.rnn import pack_padded_sequence
 import torch
 import torch.nn.functional as F
 from helper import *
+
+
 """
 Embedding(it is a super class that creates objects of other embedding classes):
 1) __init__ function: creates objects of classes a) CharLevelEmbeddingCNN b) WordLevelEmbedding
+  INPUT for __init__: config
+
+2) forward function:
   INPUTS for forward: word_tensor, char_tensor
   OUTPUTS from forward: return word_level_embedding, character_level_cnn embedding ( N x W x Dim) where N is batch size, W : sequence length
   a) CharLevelEmbeddingCNN:
-     INPUTS_from_config: is_train(boolean),keep_prob(1- drop_out_probability), kernel_dim(output channel size) , kernel_size(tuple containing (filter_height ,filter_width) ) , embedding_dim(input channel size), initial_char_embedding
+     INPUTS to __init__(from config): is_train(boolean),keep_prob(1- drop_out_probability), kernel_dim(output channel size) , kernel_size(tuple containing (filter_height ,filter_width) ) , embedding_dim(input channel size), initial_char_embedding
      INPUTS for forward ()  : char_tensor
-     OUTPUT from forward ():  return character_level_cnn embedding ( N x W x Dim) where N is batch size, W : sequence length
+     OUTPUT from forward ():  return  character_level_cnn embedding ( N x W x Dim) where N is batch size, W : sequence length
   b) WordLevelEmbedding:
-     INPUTS from config: vocab_size(int), word_embedding_size, initial_word_embedding ,padding_idx , fine_tune
+     INPUTS to __init__(from config): vocab_size(int), word_embedding_size, initial_word_embedding ,padding_idx , fine_tune
      INPUTS for forward() : word_tensor
      OUTPUTS for forward: word level embedding
 
@@ -23,59 +28,52 @@ Embedding(it is a super class that creates objects of other embedding classes):
 
 
 class Embedding_layer(nn.Module):
-    def __init__(self, config): #use_char_emb, use_word_emb,char_embed_type, is_train, keep_prob, kernel_dim , kernel_size, embedding_dim,vocab_size,
-                 #padding_idx=None, word_embedding_size = 300, fine_tune=False):
+    def __init__(self, config):
         super(Embedding,self).__init__()
 
-        # self.use_char_emb = config.use_char_emb
-        #self.use_char_emb = use_char_emb
-        #self.use_word_emb = use_word_emb
-        #self.char_embed_type = char_embed_type
-        #self.char_tensor = char_tensor
-        #self.word_tensor = word_tensor
         self.config = config
 
 
         if(self.use_char_emb == True && self.use_word_emb == True):
             self.initial_word_embedding = pickle.load(open(os.path.join(emb_dir, "word_embeddings" + str(config.word_emb_size) + ".pkl")))
             self.initial_char_embedding = pickle.load(open(os.path.join(self.emb_dir, "char_embeddings" + str(config.char_emb_size) +  ".pkl")))
-            self.char_embedding_cnn = CharLevelEmbeddingCNN(is_train, keep_prob, kernel_dim , kernel_size, embedding_dim,initial_char_embedding)
-            self.word_level_embedding = WordLevelEmbedding(vocab_size, word_embedding_size , initial_word_embedding,padding_idx, fine_tune)
+            self.char_embedding_cnn = CharLevelEmbeddingCNN(self.config,initial_char_embedding)
+            self.word_level_embedding = WordLevelEmbedding(self.config,initial_word_embedding)
 
         elif(self.use_char_emb == False && self.use_word_emb == True):
             self.initial_word_embedding = pickle.load(open(os.path.join(self.emb_dir, "word_embeddings" + str(config.word_emb_size) + ".pkl")))
-            self.word_level_embedding = WordLevelEmbedding(vocab_size, word_embedding_size, initial_word_embedding,padding_idx, fine_tune)
+            self.word_level_embedding = WordLevelEmbedding(self.config,initial_word_embedding)
 
         elif(self.use_char_emb == True && self.use_word_emb == False):
             self.initial_char_embedding = pickle.load(open(os.path.join(self.emb_dir, "char_embeddings" + str(config.char_emb_size) +  ".pkl")))
-            self.char_embedding_cnn = CharLevelEmbeddingCNN(is_train, keep_prob, kernel_dim , kernel_size, embedding_dim,initial_char_embedding)
+            self.char_embedding_cnn = CharLevelEmbeddingCNN(self.config,initial_char_embedding)
 
     def forward(self,char_tensor,word_tensor):
 
         if(self.use_char_emb == True && self.use_word_emb == True):
             char_embedding = self.char_embedding_cnn(char_tensor)
             word_embedding = self.word_level_embedding(word_tensor)
-            return char_embedding,word_embedding
+            return word_embedding,char_embedding
 
         elif(self.use_char_emb == False && self.use_word_emb == True):
             word_embedding = self.word_level_embedding(word_tensor)
-            return None, word_embedding
+            return word_embedding,None
 
         elif(self.use_char_emb == True && self.use_word_emb == False):
             char_embedding = self.char_embedding_cnn(char_tensor)
-            return char_embedding, None
+            return None,char_embedding
 
 
 class _CharLevelEmbeddingCNN(nn.Module):
 
-    def __init__(self, is_train, keep_prob, kernel_dim , kernel_size, embedding_dim,initial_char_embedding):
+    def __init__(self,config,initial_char_embedding):
         super(CharLevelEmbeddingCNN, self).__init__()
 
 
-        self.char_embed_mat_initial = nn.Embedding(self.char_vocab_size, embedding_dim=config.char_emb_size, padding_idx = padding_idx)
+        self.char_embed_mat_initial = nn.Embedding(config.char_vocab_size, embedding_dim=config.char_emb_size, padding_idx = config.padding_idx)
         self.char_embed_mat_initial.weight.data.copy_(torch.FloatTensor(initial_char_embedding))
-        self.char_embeddings = MultiConv1D(is_train, keep_prob, kernel_dim, kernel_size, embedding_dim)
-
+        self.char_embeddings = MultiConv1D(config.is_train, config.keep_prob, config.kernel_dim, config.kernel_size,config.embedding_dim)
+.
     def forward(self,char_tensor):
         # char_tensor = temp1 ( batch_size X length )
         encoded_content_char = self.char_embed_mat_initial(char_tensor)
@@ -85,13 +83,12 @@ class _CharLevelEmbeddingCNN(nn.Module):
 
 class _WordLevelEmbedding(nn.Module):
 
-    def __init__(self, vocab_size, word_embedding_size = 300, initial_word_embedding = None,
-                 padding_idx = None, fine_tune=False):
+    def __init__(self,config, initial_word_embedding):
 
-        self.word_embedding = nn.Embedding(vocab_size, embedding_dim = word_embedding_size)
+        self.word_embedding = nn.Embedding(config.vocab_size, embedding_dim = config.word_embedding_size)
         self.word_embedding.weight.data.copy(initial_word_embedding)
 
-        if (fine_tune == False):
+        if (config.fine_tune == False):
             self.word_embedding.requires_grad = False
 
     def get_embedding(self,word_tensor):
